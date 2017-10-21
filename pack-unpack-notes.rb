004469eecd17fd1a2b283c4c24c0b89d04c35a75
David Vezzani <david_vezzani@yahoo.com>
2017-10-21 12:44:47 -0600

    tiles are translating correctly

--- pack-unpack-notes.rb
+++ pack-unpack-notes.rb
@@ -21,43 +21,24 @@ def hex_length(exp)
   (parts[1].to_i(16) - parts[0].to_i(16)) + 1
 end
 
-# size = "200".to_i(16)
-# tour_content[0...size].length
-#
-# magic_number = tour_content[0..1].unpack("H4").first
-# boards_count = tour_content[2..3].unpack("C").first + 1
-# ammo = tour_content[4..5].unpack("C").first
-# gems = tour_content[6..7].unpack("C").first
-#
-# magic_num, boards_cnt_z, ammo, gems = tour_content[0..("10".to_i(16))].unpack("H4S3")
-# keys = tour_content[("08".to_i(16))..("0E".to_i(16))].unpack("C7")
-# health, board_str, torch_cnt, tcycle_cnt, ecycle_cnt, padding, score = tour_content[("0F".to_i(16))..("1C".to_i(16))].unpack("S7")
-# title_cnt = tour_content[("1D".to_i(16))..("1D".to_i(16))].unpack("C")
 
+
+
+# game_header
 keys = [:magic_num, :boards_cnt_z, :ammo, :gems, :bk, :gk, :ck, :rk, :pk, :yk, :wk, :health, :board_str, :torch_cnt, :tcycle_cnt, :ecycle_cnt, :pad_01, :score, :title_cnt, :title_x, :flg1_cnt, :flg1, :flg2_cnt, :flg2, :flg3_cnt, :flg3, :flg4_cnt, :flg4, :flg5_cnt, :flg5, :flg6_cnt, :flg6, :flg7_cnt, :flg7, :flg8_cnt, :flg8, :flg9_cnt, :flg9, :pad_02, :timeleft, :pad_03, :saved_game, :pad_04 ]
 values = tour_content[("00".to_i(16))..("200".to_i(16))].unpack("H4S3C7S7CA20CA20CA20CA20CA20CA20CA20CA20CA20CA20H20SH2CH249")
 game_header = keys.zip(values).inject({}){ |h,(k,v)| h[k] = v; h }
 
-hex_add_dec("200", 1)
-
+# board_header
 start = ("200".to_i(16))
 board_size = tour_content[start..start+1].unpack("S").first #5138
 board_stop = hex_add_dec("200", board_size+1).to_i(16)
-#board_stop = hex_add_dec("201", 5138) #1613
-#hex_length("03-23") #33
 
 keys = [:board_size, :title_cnt, :title, :pad_01]
 values = tour_content[(start+2)..board_stop].unpack("CA33H16").unshift(board_size)
 board_header = keys.zip(values).inject({}){ |h,(k,v)| h[k] = v; h }
 
-# hex_length("00-33") #52
-# hex_length("24-33") #16
-#
-# tour_content[("202".to_i(16))..("1613".to_i(16))].unpack("CA33H16")
-#
-# hex_length("00-34") #53
-# hex_add_dec("200", 53) #235
-
+# board_tiles
 start = ("200".to_i(16))
 
 max_tile_cnt = 60*25 #1500
@@ -67,18 +48,26 @@ tile_cnt = 0
 tiles = []
 
 while (tile_cnt < max_tile_cnt)
-  repeat_cnt, code, colour = tour_content[start..board_stop].unpack("Caa")
+  repeat_cnt, raw_code, raw_data = tour_content[start..board_stop].unpack("CaA")
+
+  code = raw_code.unpack('H*')[0]
+  decimal_code = code.to_i(16)
+  colour = text = nil
+  if (decimal_code >= "2f".to_i(16))
+    text = raw_data
+    tiles << [repeat_cnt, code, text]
+  else
+    colour = raw_data.unpack('H*')[0]
+    tiles << [repeat_cnt, code, colour]
+  end
+
   tile_cnt += repeat_cnt
-  ("#{code}".to_i(16))
-  tiles << [repeat_cnt, code.unpack('H*')[0], colour.unpack('H*')[0]]
   puts "<< #{tile_cnt}, #{tiles.last}"
+  # File.open("./chk.txt", "a"){|f| f.write "<< #{tile_cnt}, #{tiles.last}\n" }
   start += 3
 end
 
-# start.to_s(16)
-hex_length("08-41")
-hex_length("46-55")
-
+# board_info
 keys = [:max_shots, :darkness, :bn, :bs, :bw, :be, :reenter, :message_len, :message, :pad_01, :time_limit, :pad_02, :obj_cnt]
 values = tour_content[start..board_stop].unpack("C8H58H2SH16SSSSSSSSSSSSSSSSS")
 game_board = keys.zip(values).inject({}){ |h,(k,v)| h[k] = v; h }
